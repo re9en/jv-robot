96eb6cf9382d06aa3b96d919bba4f2a43bdd92b5
Timofii Kniahnytskiy <regen69neger@gmail.com>
2025-11-11 21:09:00 +0200

    added main class, implemented logic to RobotRoute

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,44 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        Direction currentDirection = robot.getDirection();
+
+        if (robot.getX() > toX) {
+            while (currentDirection.equals(Direction.LEFT)) {
+                robot.turnLeft();
+            }
+            while (robot.getX() != toX) {
+                robot.stepForward();
+            }
+            System.out.println("X is finished");
+        } else if (robot.getX() < toX) {
+            while (currentDirection.equals(Direction.RIGHT)) {
+                robot.turnLeft();
+            }
+            while (robot.getX() != toX) {
+                robot.stepForward();
+            }
+
+        }
+        if (robot.getY() > toY) {
+            while (currentDirection.equals(Direction.DOWN)) {
+                robot.turnLeft();
+            }
+            while (robot.getY() != toY) {
+                robot.stepForward();
+            }
+            System.out.println("Y is finished");
+        } else if (robot.getY() < toY) {
+            while (currentDirection.equals(Direction.UP)) {
+                robot.turnLeft();
+            }
+            while (robot.getX() != toY) {
+                robot.stepForward();
+            }
+
+            if ((robot.getX() == toX) && (robot.getY() == toY)) {
+            System.out.println("Y and X is finished");
+            }
+        }
     }
 }
